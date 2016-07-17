e37d24e2e631ca16fc064c1efe56ba58ddcc8e4c
Franta Polach <frantisek.polach@gmail.com>
2016-07-17 17:48:00 +0200

    28

--- 28.cs
+++ 28.cs
@@ -1,1 +1,32 @@
-to be done
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution 
+{
+    static void Main(String[] args)
+    {
+        int T = Int32.Parse(Console.ReadLine());
+        for (int j = 0; j < T; j++)
+            {
+            int N = Int32.Parse(Console.ReadLine());
+            int elem = 1;
+            int step = 2;
+            int corner = 0;
+            int sum = 1;
+            while (step < N-2 || corner != 4)
+                {
+                if (corner == 4)
+                    {
+                    step += 2;
+                    corner = 0;
+                }
+                elem += step;
+                sum += elem;
+                corner++;
+            }
+            Console.WriteLine(sum % 1000000007);
+            }
+    }
+}
+
